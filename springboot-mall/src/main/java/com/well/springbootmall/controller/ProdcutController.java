a1ffb2ac9c925e83abf1ac72f8dab7f2aa7ea9ad
WELL\USER <wellpen811006@gmail.com>
2023-08-03 17:09:33 +0800

    商品功能 - 實作查詢商品列表(分頁 + 總比數)

--- springboot-mall/src/main/java/com/well/springbootmall/controller/ProdcutController.java
+++ springboot-mall/src/main/java/com/well/springbootmall/controller/ProdcutController.java
@@ -5,6 +5,7 @@ import com.well.springbootmall.dto.ProductQueryParams;
 import com.well.springbootmall.dto.ProductRequest;
 import com.well.springbootmall.model.Product;
 import com.well.springbootmall.service.ProductService;
+import com.well.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -25,7 +26,7 @@ public class ProdcutController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
           // 查詢條件 Filtering
           @RequestParam(required = false) ProductCategory category,
           @RequestParam(required = false) String search,
@@ -49,8 +50,19 @@ public class ProdcutController {
         productQueryParams.setOffset(offset);
 
 
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
--- springboot-mall/src/main/java/com/well/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/well/springbootmall/service/ProductService.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public interface ProductService {
 
+    Integer countProduct(ProductQueryParams productQueryParams);
     List<Product> getProducts(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
